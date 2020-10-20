06a3f88f4aa69892d845d60bccf0d92ed1fb757b
nick-olesen <70233329+nick-olesen@users.noreply.github.com>
2020-10-19 23:37:24 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+//***************************************
+//***************************************
+/*
+* Nicholas Olesen
+* C++ 2020
+* Due: October 19th 2020
+* Lab 4 Temperature Output
+* This program will take temperatures from a file and output them to a bar chart.Then it will be able to determine if a temperatue is valid, 
+then it will end the program if the file stream is invalid.    			
+*/
+//****************************************
+//****************************************
+
+
+#include <iostream>
+#include <fstream> 
+#include <string> //This is used to store stars in a string
+#include <iomanip> 
+#include <cctype> //This is for finding bugs in the code
+
+
+using namespace std; 
